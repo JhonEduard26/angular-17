b975e8dfdc8d4d0b5c5507ad66d0a1ba5036c48d
jhon <jhon.bocanegra26@gmail.com>
2024-02-06 21:49:01 -0500

    add: task model

--- todoapp/src/app/pages/home/home.component.ts
+++ todoapp/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,55 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar angular CLI',
-    'Crear proyecto',
-    'Crear componente',
-    'Crear servicio',
-    'Aprender Javascript',
-  ])
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Aprender Angular',
+      completed: false,
+    },
+    {
+      id: Date.now() + 1,
+      title: 'Aprender TypeScript',
+      completed: true,
+    },
+    {
+      id: Date.now() + 2,
+      title: 'Aprender Firebase',
+      completed: false,
+    },
+  ]);
+
+  handleAddTask(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = {
+      id: Date.now(),
+      title: input.value.trim(),
+      completed: false,
+    }
+
+    if (!newTask.title) {
+      return;
+    }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+
+    input.value = '';
+  }
+
+  values = Array.from({ length: 1000 }, (_, i) => i);
+
+  handleDeleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.toSpliced(index, 1))
+  }
+
+  handleCompleteTask(index: number) {
+    this.tasks.update((tasks) => {
+      const updatedTask = [...tasks];
+      const taskToUpdate = updatedTask[index];
+
+      if (taskToUpdate) {
+        taskToUpdate.completed = !taskToUpdate.completed;
+      }
+      return updatedTask
+    })
+  }
 }
