865501cf534e7076354ba9dcd4d3014b47bb44fa
jhon <jhon.bocanegra26@gmail.com>
2024-02-18 21:19:43 -0500

    add: reactive forms

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -24,6 +25,13 @@ export class LabsComponent {
     'Crear el build de la app',
   ])
 
+  person = signal({
+    name: 'Jhon',
+    age: 24
+  })
+
+  colorCtrl = new FormControl('')
+
   handleClick() {
     console.log('Click')
   }
@@ -37,5 +45,16 @@ export class LabsComponent {
     const input = event.target as HTMLInputElement
     const newValue = input.value
     this.name.set(newValue)
+  }
+
+  handleInputSignal(event: Event) {
+    const input = event.target as HTMLInputElement
+    const newValue = input.value
+    this.person.update((obj) => {
+      return {
+        ...obj,
+        name: newValue
+      }
+    })
   }
 }
