69558eb0ff1a6847d2b7aeb619376dd6d0c0a06a
jhon <jhon.bocanegra26@gmail.com>
2024-02-24 17:01:18 -0500

    add: new components and update existing components

--- store/src/app/domains/products/components/product/product.component.ts
+++ store/src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input({required: true}) productImg = '';
+  @Input({required: true}) title = '';
+  @Input({required: true}) price = 0;
 
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    this.addToCart.emit('Mensaje desde el hijo al padre' + this.title);
+  }
 }
