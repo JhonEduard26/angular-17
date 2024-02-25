f17dc02fec4bc8aeb5424ff604f1abdb0857c967
jhon <jhon.bocanegra26@gmail.com>
2024-02-25 18:03:03 -0500

    refactor: header component and cart service

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, Output, SimpleChanges, signal } from '@angular/core';
+import { Component, Input, Output, SimpleChanges, inject, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -9,21 +10,12 @@ import { Product } from '../../models/product.model';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Input({ required: true }) items!: Product[];
   hideSideMenu = signal(true);
-  total = signal(0);
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['items']) {
-      this.total.set(this.getTotal());
-    }
-  }
+  private cartService = inject(CartService);
+  items = this.cartService.cart;
+  total = this.cartService.total;
 
   toggleSideMenu() {
     this.hideSideMenu.update((prev) => !prev);
-  }
-
-  getTotal() {
-    return this.items.reduce((acc, item) => acc + item.price, 0);
   }
 }
--- store/src/app/domains/shared/services/cart.service.ts
+++ store/src/app/domains/shared/services/cart.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart = signal<Product[]>([]);
+  total = computed(() => this.cart().reduce((acc, product) => acc + product.price, 0));
 
   constructor() { }
+
+  addToCart(product: Product) {
+    this.cart.update((products) => [...products, product]);
+  }
 }
