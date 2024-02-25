3000b0c8f4f88332fd177f2fe91e3110eedc0720
jhon <jhon.bocanegra26@gmail.com>
2024-02-24 19:47:00 -0500

    add: wavesurfer.js library and about page

--- store/src/app/domains/shared/components/counter/counter.component.ts
+++ store/src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  @Input({ required: true }) duration = 0;
+  @Input({ required: true }) message = "";
+  counter = signal(0)
+  counterRef: number | undefined
 
+  constructor() {
+    // No async
+    // before render
+    console.log('CounterComponent constructor');
+    console.log('-'.repeat(12));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('CounterComponent ngOnChanges');
+    console.log('-'.repeat(12));
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    // before render
+    // exa: for api calls
+    console.log('CounterComponent ngOnInit');
+    console.log('-'.repeat(12));
+
+    this.counterRef = window.setInterval(() => {
+      console.log('interval: ');
+      this.counter.update(prev => prev + 1);
+    }, 1000)
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // after child component render
+    console.log('CounterComponent ngAfterViewInit');
+    console.log('-'.repeat(12));
+  }
+
+  ngOnDestroy() {
+    // before destroy
+    console.log('CounterComponent ngOnDestroy');
+    console.log('-'.repeat(12));
+    window.clearInterval(this.counterRef)
+  }
 }
