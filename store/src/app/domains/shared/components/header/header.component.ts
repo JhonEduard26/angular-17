f907a496428f51b53a259310f0bee5bb068ec117
jhon <jhon.bocanegra26@gmail.com>
2024-02-25 20:39:49 -0500

    update: header component HTML and TypeScript files

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { CartService } from '@shared/services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
