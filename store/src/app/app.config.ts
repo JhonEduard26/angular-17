9ff46d80842b600b346f5d74e44295461fb94d7e
jhon <jhon.bocanegra26@gmail.com>
2024-02-28 21:03:44 -0500

    update: app.config.ts and app.routes.ts

--- store/src/app/app.config.ts
+++ store/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)), provideHttpClient()]
 };
